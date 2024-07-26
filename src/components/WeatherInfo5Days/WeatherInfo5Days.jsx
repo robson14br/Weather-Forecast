26b667122bfea5aae0b8ecd93b212dec1923c304
Robson <robson14br@outlook.com>
2024-07-25 23:04:58 -0300

    style 5days

--- src/components/WeatherInfo5Days/WeatherInfo5Days.jsx
+++ src/components/WeatherInfo5Days/WeatherInfo5Days.jsx
@@ -1,41 +1,47 @@
 /* eslint-disable react/prop-types */
-import './WeatherInfo5Days.css'
+import "./WeatherInfo5Days.css";
 
 function WeatherInfo5Days({ weather5Days }) {
-  
-  let dailyForecast = {}
+  let dailyForecast = {};
 
-  for(let forecast of weather5Days.list) {
-     const date = new Date(forecast.dt*1000).toLocaleDateString()
+  for (let forecast of weather5Days.list) {
+    const date = new Date(forecast.dt * 1000).toLocaleDateString();
 
-     if(!dailyForecast[date]){
-      dailyForecast[date] = forecast
-     }
+    if (!dailyForecast[date]) {
+      dailyForecast[date] = forecast;
+    }
   }
 
-  const nextFiveDays = Object.values(dailyForecast).slice(0,6)
+  const nextFiveDays = Object.values(dailyForecast).slice(0, 6);
 
-  function convertDate(date){
-    const newDate = new Date(date.dt * 1000).toLocaleTimeString('pt-Br', {weekday: 'long', day: '2-digit'})
-
-    return newDate
-  }
+  function convertDate(date) {
+    const newDate = new Date(date.dt * 1000).toLocaleDateString("pt-BR", {
+        weekday: "long",
+        day: "2-digit"
+    });
+    return newDate;
+}
 
   return (
-    <div className='weather-container'>
-      <p>Previsão Próximos 5 days</p>
-      {nextFiveDays.map(forecast => ( 
-        <div key={forecast.dt}>
+    <div className="weather-container">
+      <h3>Previsão Próximos 5 days</h3>
+      <div className="weather-list">
+        {nextFiveDays.map((forecast) => (
+          <div key={forecast.dt} className="weather-item">
+            <p className="forecast-day">{convertDate(forecast)}</p>
 
-          <p>{convertDate(forecast)}</p>
+            <img
+              src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
+            />
 
-          <img src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}/>
-
-          <p>{forecast.weather[0].description}</p>
-          <p>{Math.round(forecast.main.temp_min)}ºC min / {Math.round(forecast.main.temp_max)}ºC máx</p>
-        </div>
-        
-      ))}
+            <p className="forecast-description">{forecast.weather[0].description}</p>
+            <p>
+              {Math.round(forecast.main.temp_min)}ºC min /{" "}
+              {Math.round(forecast.main.temp_max)}ºC máx
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
